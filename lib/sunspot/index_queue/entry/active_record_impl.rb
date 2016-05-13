95fc9d0a3cafb3eef33c5092511e11a133b58777
Manuel Alejandro <manuel.alejandro@workshare.com>
2016-05-13 16:52:13 +0100

    [37906] Silencing error and logging it instead of raising unnecessary airbrakes

--- lib/sunspot/index_queue/entry/active_record_impl.rb
+++ lib/sunspot/index_queue/entry/active_record_impl.rb
@@ -20,7 +20,7 @@ module Sunspot
       # as the primary key, you can add additional statements to the migration to do so.
       class ActiveRecordImpl < ActiveRecord::Base
         include Entry
-        
+
         self.table_name = "sunspot_index_queue_entries"
 
         before_save :set_queue_position_column
@@ -29,8 +29,8 @@ module Sunspot
 
           # From MYSQL doc:
           # ---------------------------------------------------------------------
-          # In some cases, MySQL cannot use indexes to resolve the ORDER BY, 
-          # although it still uses indexes to find the rows that match the 
+          # In some cases, MySQL cannot use indexes to resolve the ORDER BY,
+          # although it still uses indexes to find the rows that match the
           # WHERE clause. These cases include the following:
           #
           # - You use ORDER BY on different keys:
@@ -40,12 +40,12 @@ module Sunspot
           #
           # The described condition applies to our query to sort the elements to
           # be processed since the conditions are to sort by priority DESC and
-          # run_at ASC. To boost database performance a new column has been 
-          # added containing both informations encoded in such a fashion that a 
-          # sort applied on a set of entries returns the entries in the expected 
+          # run_at ASC. To boost database performance a new column has been
+          # added containing both informations encoded in such a fashion that a
+          # sort applied on a set of entries returns the entries in the expected
           # order. The column is called queue_position and the format of its
           # values is:
-          # 
+          #
           # "2.0000000000-0001423053863"
           #
           # Which represents the inverse value of the priority - the unix time of
@@ -54,7 +54,7 @@ module Sunspot
           def calculate_queue_position_column(priority, run_at)
             raise "WTF!! Priority should not be smaller than -10" if priority < -10
             raise "Priority should not be bigger than 10^4" if priority > 10000
-            
+
             priority = priority + 10
             inverse_priority = priority == 0 ? 2 : 1/priority.to_f
             fixed_width_time = run_at.to_i.to_s[0...13].rjust(13, '0')
@@ -67,7 +67,7 @@ module Sunspot
             conditions = queue.class_names.empty? ? {} : {:record_class_name => queue.class_names}
             count(:conditions => conditions)
           end
-          
+
           # Implementation of the ready_count method.
           def ready_count(queue)
             conditions = basic_conditions
@@ -107,8 +107,8 @@ module Sunspot
             conditions = queue.class_names.empty? ? {} : {:record_class_name => queue.class_names}
             update_all({:run_at => Time.now.utc, :attempts => 0, :error => nil, :lock => 0}, conditions)
           end
-         
-          # Implementation of the next_batch! method. 
+
+          # Implementation of the next_batch! method.
           def next_batch!(queue)
             conditions = basic_conditions
             unless queue.class_names.empty?
@@ -152,12 +152,12 @@ module Sunspot
           rescue ActiveRecord::RecordNotUnique => e
             false
           end
-          
+
           # Implementation of the delete_entries method.
           def delete_entries(entries)
             delete_all(:id => entries)
           end
-          
+
           # Create the table used to store the queue in the database.
           def create_table
             connection.create_table table_name do |t|
@@ -178,18 +178,20 @@ module Sunspot
 
         # Implementation of the set_error! method.
         def set_error!(error, retry_interval = nil)
-          self.attempts += 1
-          self.run_at = (retry_interval * attempts).from_now.utc if retry_interval
-          self.error = "#{error.class.name}: #{error.message}\n#{error.backtrace.join("\n")[0, 4000]}"
-          self.priority -= 1
-          self.lock = 0
-
-          logger.warn(error)
-          discard! if discard?
-
           begin
             save!
           rescue => e
+            unless destroyed?
+              self.attempts += 1
+              self.run_at = (retry_interval * attempts).from_now.utc if retry_interval
+              self.error = "#{error.class.name}: #{error.message}\n#{error.backtrace.join("\n")[0, 4000]}"
+              self.priority -= 1
+              self.lock = 0
+
+              logger.warn(error)
+              discard! if discard?
+            end
+
             if logger
               logger.warn(e)
             end
